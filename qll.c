ce263de2ed93bd0176f4e5d52de5bb3986477eb1
twiliHarsh <harshd403@gmail.com>
2019-11-03 22:35:39 +0530

    new

--- qll.c
+++ qll.c
@@ -1,35 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-struct node {
-    int info ;
-    struct node *next;
-
-};
-
-struct node *front = NULL;
-struct node *rear = NULL;
-
-void insert_end()
+struct Node
 {
-    struct node *p;
-    p=(struct node*)malloc(sizeof(struct node));
-    if (p==NULL)
-    printf("Overflow");
-    else
-    {
-        if (front == NULL && rear == NULL)
-        {
-            front = rear ;
-        }
-    }else 
-    {
-        rear -> next = p;
-        rear = p;
-        rear -> next = NULL;
+   int data;
+   struct Node *next;
+}*front = NULL,*rear = NULL;
 
-    }
+void insert(int);
+void delete();
+void display();
+
+void main()
+{
+   int choice, value;
+   
+   printf("\n:: Queue Implementation using Linked List ::\n");
+   while(1){
+      printf("\n****** MENU ******\n");
+      printf("1. Insert\n2. Delete\n3. Display\n4. Exit\n");
+      printf("Enter your choice: ");
+      scanf("%d",&choice);
+      switch(choice){
+	 case 1: printf("Enter the value to be insert: ");
+		 scanf("%d", &value);
+		 insert(value);
+		 break;
+	 case 2: delete(); break;
+	 case 3: display(); break;
+	 case 4: exit(0);
+	 default: printf("\nWrong selection!!! Please try again!!!\n");
+      }
+   }
 }
-
-
-
+void insert(int value)
+{
+   struct Node *newNode;
+   newNode = (struct Node*)malloc(sizeof(struct Node));
+   newNode->data = value;
+   newNode -> next = NULL;
+   if(front == NULL)
+      front = rear = newNode;
+   else{
+      rear -> next = newNode;
+      rear = newNode;
+   }
+   printf("\nInsertion is Success!!!\n");
+}
+void delete()
+{
+   if(front == NULL)
+      printf("\nQueue is Empty!!!\n");
+   else{
+      struct Node *temp = front;
+      front = front -> next;
+      printf("\nDeleted element: %d\n", temp->data);
+      free(temp);
+   }
+}
+void display()
+{
+   if(front == NULL)
+      printf("\nQueue is Empty!!!\n");
+   else{
+      struct Node *temp = front;
+      while(temp->next != NULL){
+	 printf("%d--->",temp->data);
+	 temp = temp -> next;
+      }
+      printf("%d--->NULL\n",temp->data);
+   }
+}
